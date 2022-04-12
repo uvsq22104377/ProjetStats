26aba85e360ddae4fc476f5885193418d8c07a97
xinlei YANG <xinlei.yang@ens.uvsq.fr>
2022-04-12 12:30:49 +0200

    travail effectue le 12/04

--- projet.py
+++ projet.py
@@ -2,11 +2,31 @@ import random as rd
 
 
 def cree_fichier_alea(nb, nomfichier):
-    nomfichier = open("fichier de texte", "a","w")
-    for i in range(2):
-        temp = rd.uniform(0, 500)
-        nomfichier.write(temp, " ")
-    nomfichier.write("\n")
+    global nb
+    nomfichier = open("fichier de texte", "w")
+    for i in range(nb):
+        for j in range(2):
+            temp = rd.uniform(0, 500)
+            nomfichier.write(temp, " ")
+        nomfichier.write("\n")
 
 
 def lit_fichier(nomfic):
+    nomfic = open(nomfic,'r')
+    liste = nomfic.split()
+    liste_X = []
+    liste_Y = []
+    for i in range(nb):
+        if i % 2:
+            liste_X.append(liste[i])
+        else:
+            liste_Y.append(liste[i])
+    return liste_X,liste_Y
+
+
+def trace_Nuage(nomf):
+    lit_fichier(nomf)
+    pass
+
+
+
