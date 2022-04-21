a944d197f8623c3b287cfb0b8aebf2136e2a6461
xinlei YANG <xinlei.yang@ens.uvsq.fr>
2022-04-21 18:08:01 +0200

    travail effectue le 21/04

--- projet.py
+++ projet.py
@@ -1,8 +1,16 @@
 import random as rd
+import tkinter as tk
+
+t = 0
+HEIGHT = 500
+WIDTH = 500
+liste_X = []
+liste_Y = []
 
 
 def cree_fichier_alea(nb, nomfichier):
-    global nb
+    t = nb
+    global t
     nomfichier = open("fichier de texte", "w")
     for i in range(nb):
         for j in range(2):
@@ -12,21 +20,40 @@ def cree_fichier_alea(nb, nomfichier):
 
 
 def lit_fichier(nomfic):
+    global liste_X,liste_Y
+    liste = []
     nomfic = open(nomfic,'r')
-    liste = nomfic.split()
-    liste_X = []
-    liste_Y = []
-    for i in range(nb):
-        if i % 2:
-            liste_X.append(liste[i])
-        else:
-            liste_Y.append(liste[i])
-    return liste_X,liste_Y
+    while True:
+        texte = nomfic.readline()
+        liste.append(texte.split())
+        if texte == '':
+            break
+    for i in range(t):
+        liste_X.append(liste[i][0])
+        liste_Y.append(liste[i][1])
 
 
-def trace_Nuage(nomf):
+def trace_nuage(nomf):
     lit_fichier(nomf)
-    pass
+    for m, n in liste_X, liste_Y:
+        cv.create_line((m, n), (m+1, n+1), width=1)
+    return len(liste_X)
+
+
+def trace_droite(a, b):
+    cv.create_line((0, b), (500, 500*int(a)+int(b)))
+
+
+racine = tk.Tk()
+cv = tk.Canvas(racine, height=HEIGHT, width=WIDTH)
+
+
+
+
+
+
+
+
 
 
 
